3137065de2a5cfa1a8fa01817b10784f19dc1c9a
Matis Leima <matisleima@gmail.com>
2024-02-20 13:22:30 +0200

    WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <h1>MAIN</h1>
+
     <div id="background-container">
       <div class="clickable-area" @click="goToView('musicRoute')" style="top: 0.9%; left: 3%; width: 50%; height: 50%;"></div>
-      <div class="clickable-area" @click="goToView('artRoute')" style="top: 77%; left: 18%; width: 7.5%; height: 9%;"></div>
+      <div class="clickable-area" @click="goToView('portfolioRoute')" style="top: 77%; left: 18%; width: 7.5%; height: 9%;"></div>
       <div class="clickable-area" @click="goToView('gamesRoute')" style="top: 49.5%; left: 51.8%; width: 4%; height: 7.3%;"></div>
       <div class="clickable-area" @click="goToView('bioRoute')" style="top: 29.5%; left: 90.2%; width: 6%; height: 28.2%;"></div>
 
@@ -14,7 +16,6 @@
 </template>
 
 <script>
-// import '@/assets/css/my-style.css';
 
 export default {
   methods: {
@@ -25,3 +26,4 @@ export default {
 }
 
 </script>
+
